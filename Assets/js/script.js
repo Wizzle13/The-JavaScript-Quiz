93dd8f3d63de1116311b2b03f0b81b0c1d074db6
CBurton <burton.chris13@gmail.com>
2022-04-02 16:42:51 -0500

    completion of project

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,6 +2,7 @@ var quizTimer = 100;
 var timerEl = document.getElementById('timer');
 var i = 0;
 var quizStarted = "no";
+var score = "0";
 
 //  quiz/game timer section
 var countDown =function(){
@@ -26,16 +27,34 @@ var countDown =function(){
 var questions = 
       [
         {
-          question: "What is the capital of United Kingdom?",
-          choices: ["Manchester", "Birmingham", "London", "Iowa"],
-          answer: "2"
+          question: "If you save your array of objects to the browser’s local storage and it looks like [Object object] when you visit it in Chrome’s DevTools, what’s wrong?",
+          choices: ["The array wasn’t stringified with JSON.stringify() before saving it in Local Storage.", "The array wasn’t stringified with JSON.stringify() before saving it in Local Storage."],
+          answer: "1"
         },
         
         {
-          question: "What is the capital of United States?",
-          choices: ["California", "New York", "Miami", "Florida"],
+          question: "In the DOM’s event object, what does its target property refer to?",
+          choices: ["It refers to the HTML element that was interacted with to dispatch the event.", "It refers to the HTML element we want to affect as a result of our dispatched event"],
           answer: "1"
-        }
+        },
+
+        {
+            question: "What does event.preventDefault() do?",
+            choices: ["It stops the browser from reloading the page upon a form submission","It stops the browser from allowing the form submission event to occur. "],
+            answer: "1"
+        },
+
+        {
+            question: "The browser event submit allows us to do the following:",
+            choices: ["Submit a form using a button.", "Submit a form using both a button and the Enter key."],
+            answer: "2"
+        },
+
+        {
+            question: "How do we use JavaScript to get the information entered into a form’s input field?",
+            choices: ["We can select the form’s input element and use the value property to read its data.", "We can select the form itself and use the value property to read all of its data.", "We can select the form’s input element and use the textContent or innerHTML properties to read its data."],
+            answer: "1"
+        },  
     ];
 
 // loads question
@@ -52,38 +71,46 @@ var loadQuestion = function(){
         for ( var opt = 0; opt < options.length; opt++) {
         
      
-            formEl.querySelector("#quizAnwsers").innerHTML = "<input type='radio' name='Anwser' value='1' />"+ options[0]+"<br/><input type='radio' name='Anwser' value='2' />"+ options[1]+"<br/><input type='radio' name='Anwser' value='3' />"+ options[2]+"<br/><input type='radio' name='Anwser' value='4' />"+ options[3]+"<br/>"
+            formEl.querySelector("#quizAnwsers").innerHTML = "<input type='radio' name='Anwser' value='1' />"+ options[0]+"<br/><input type='radio' name='Anwser' value='2' />"+ options[1]+"<br/><br/>"
        
-            
-            console.log(opt);
-            console.log(options[opt]);
         }
     }
     else {
-        formEl.querySelector("#questions").textContent = ("Congratulation you completed the test! Your Score is "+quizTimer);
-        formEl.querySelector("#quizAnwsers").innerHTML = "<input type='input' name='HighScore'  />";
+        quizStarted = "HS";
+        score = quizTimer;
+        formEl.querySelector("#questions").textContent = ("Congratulation you completed the test! Your Score is "+score);
+        formEl.querySelector("#quizAnwsers").innerHTML = "<input type='input' name='HighScore' class ='HSI' />";
         formEl.querySelector("#button").innerHTML = "<button type='submit' id = 'submit'>Submit Score</button>"
     }
 }
+
+// Submit the High Score
+var submitHighScore =function() {
+    var initials =$("#HSI").val();
+    localStorage.setItem('HighScore', initials +" , "+ score);
+    window.location.reload(true);
+}
+
+// check the anwser
 var checkAnwser = function(event) {
     // event.preventDefault();
     var qAnwser = questions[i].answer;
-    console.log(qAnwser);
+    
     var ele = document.getElementsByName('Anwser');
                   
     for(var e = 0; e < ele.length; e++) {
-        if(ele[e].checked)
-        console.log(ele[e].value);
+        if(ele[e].checked){
+        
         var submitedAnwser = ele[e].value
+        }
+        
     }
 
-    
-    if(qAnwser === submitedAnwser){
-        console.log("correct");
+    // checks to see if the anwser
+    if(qAnwser == submitedAnwser){
         quizTimer = quizTimer + 5;
     }
     else {
-        console.log("incorrect");
         quizTimer = quizTimer - 10;
     }
     
@@ -92,19 +119,33 @@ var checkAnwser = function(event) {
     loadQuestion();
 }
 
+// starts the quiz
 var startQuiz = function(event) {
     event.preventDefault();
     countDown();
     if (quizStarted === "no"){
         loadQuestion();
     }
-    else {
+    else if (quizStarted === "yes"){
         checkAnwser();
     }    
+    else {
+        submitHighScore();
+    }
+}
+
+var showHS = function(){
+    quizStarted = "HS";
+    var CurrentHS = localStorage.getItem("HighScore")
+    formEl.querySelector("#questions").textContent = ("The Current High Score is:");
+    formEl.querySelector("#quizAnwsers").innerHTML = "<h3>"+ CurrentHS +"</h3>";
+    formEl.querySelector("#button").innerHTML = "<button type='submit' id = 'submit'>Close</button>"
 }
     
 var formEl = document.querySelector("#quiz-form");
     formEl.querySelector("#button").innerHTML = "<button type='submit' id = 'submit'>Start The Test!</button>"
     formEl.addEventListener("submit", startQuiz);
+var formE2 = document.querySelector("#highScore");   
+    formE2.addEventListener("click", showHS);
     
 
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # The-JavaScript-Quiz
+
+This web site will allow you to take a timed quiz and submit your high score.
+
+This site was built using the following:
+
+![HTML5](https://img.shields.io/badge/HTML5-E34F26?style=plastic&logo=html5&logoColor=white)
+![CSS](https://img.shields.io/badge/CSS3-1572B6?style=plastic&logo=css3&logoColor=white)
+![JavaScript](https://img.shields.io/badge/-JavaScript-F7DF1E?style=plastic&logo=Javascript&logoColor=white)
+
+![Screen Shot](./Assets/images/screenshot.jpg)
+
+### Links:
+
+GitHub Repository: https://github.com/Wizzle13/The-JavaScript-Quiz
+
+GitHub URL:https://wizzle13.github.io/The-JavaScript-Quiz/
